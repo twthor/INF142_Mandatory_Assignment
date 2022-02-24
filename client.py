379dd5e08b68a68a4be5add2b6850abbb5be8b38
sofiaburkow <63155724+sofiaburkow@users.noreply.github.com>
2022-02-24 08:54:30 +0100

    initiated client socket

--- client.py
+++ client.py
@@ -1,2 +1,11 @@
 # Client side of TNT
 # test push
+from socket import socket
+
+sock = socket()
+sock.connect(('localhost', 5000))
+
+message = ''
+sock.sendall(message.encode())
+response = sock.recv(2048)
+print('From server:', response.decode())
