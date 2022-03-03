6e52bfd2ccc4cd19116ece1438e9b0bc4913bc6a
Tobias.W.Thorsen <tobias.w.thorsen@student.uib.no>
2022-03-03 08:45:20 +0100

    MongoDB working

--- mongoDB.py
+++ mongoDB.py
@@ -12,24 +12,24 @@ print(pymongo.version)
 # Get you password from .env file
 password = os.environ.get("PASSWORD")
 username = "admin"
-clusterName = "inf142-ma"
+clusterName = "inf142-cluster"
 
 # Connect to your cluster
-client = MongoClient("mongodb+srv://"+ username + ":" + password + "@"+ clusterName + ".3w00x.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://"+ username + ":" + password + "@"+ clusterName + ".mi4zj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
 
 # Create a new database in your cluster
-database = client.INF142
-# database = client.sample_airbnb
-# databaseChamps = client.Champions
-# Create a new collection in you database
-person = database.person
-# champions = databaseChamps.champsList
+database = client.Champions
 
-personDocument = {
-  "firstname": "Tobias",
-  "lastname": "Thorsen",
-  "course": "INF142"
-}
-print(personDocument)
-person.insert_one(personDocument)
-print(person.inserted_id)
+# Create a new collection in you database
+champStats = database.Champions
+stats = {}
+
+# databaseChamps = client.Champions
+with open("some_champs.txt") as f:
+  for line in f:
+    line = line.split(",")
+    stats[line[0]] = [line[1], line[2], line[3][:-1]]
+
+champStats.insert_one(stats)
+
